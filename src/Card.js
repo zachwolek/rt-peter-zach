522ea6d4c1cf15187ab18633cf7bc2ca80721ca6
Peter Kim <peterkim.pk1@gmail.com>
2024-07-07 16:42:49 -0400

    Added conditional checking for average_rating to set the card id

--- src/Card.js
+++ src/Card.js
@@ -3,13 +3,22 @@ import moment from 'moment'
 import PropTypes from 'prop-types';
 
 export default function Card({id, img, release_date, title, average_rating, updateSingleMovie}) {
-    
+    let coloredCard;
+    if (average_rating >= 7) {
+        coloredCard = 'green'
+    }
+    else if (average_rating >= 4 && average_rating < 7) {
+        coloredCard = 'yellow'
+    }
+    else if (average_rating < 4) {
+        coloredCard = 'red'
+    }
     return (
-        <div className='card' key={id}>
+        <div className='card' key={id} id={coloredCard}>
             <img src={img} alt={title}/>
             <h2>{title}</h2>
             <p>Released: {moment(release_date).format('MMM YYYY')}</p>
-            <p>Rating: {average_rating.toFixed(1)}/10</p>
+            <p className='average_rating'>Rating: {average_rating.toFixed(1)}/10</p>
             <button onClick={()=> {
                 console.log("ID CLICK", id)
                 updateSingleMovie(id)
