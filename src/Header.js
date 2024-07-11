ac0b188e1c22af57fb0135b2ebf2d42dad1f767f
Peter Kim <peterkim.pk1@gmail.com>
2024-07-11 18:17:00 -0400

    feat: added prop checking for each prop. created handleFilter function to copy the data array and filter on each onChange event. Then sets the filtered data as movie data on App.
    Co-Authored-By: Zach Wolek <zach.wolek@gmail.com>
    Co-Authored-By: Peter Kim <peterkim.pk1@gmail.com>

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,35 @@
 import './Header.css'
+import  PropTypes from 'prop-types';
 
-export default function Header() {
+export default function Header({filteredMovies, updateFilteredMovies}) {
+    function handleFilter(input) {
+        const matchingMovies = filteredMovies.map(movie => {
+           return movie.title.toLowerCase().includes(input) ? {...movie} : {}
+    })
+    const allFilteredMovies = matchingMovies.filter(movie => Object.keys(movie).length !== 0)
+    updateFilteredMovies(allFilteredMovies)
+    }
+
     return (
         <nav>
-            <div className='app-title'>Rancid Tomatillos</div>
+            <h1 className='app-title'>Rancid Tomatillos</h1>
+            <div className='forms'>
+                <input placeholder='Search By Title'className='search' onChange={(e) => handleFilter(e.target.value)
+                }></input>
+                <button>Search</button>
+            </div>
         </nav>
     )
+}
+
+Header.propTypes = {
+    updateFilteredMovies: PropTypes.func.isRequired,
+    filteredMovies: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        poster_path: PropTypes.string.isRequired,
+        average_rating: PropTypes.number.isRequired,
+        release_date: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        backdrop_path: PropTypes.string.isRequired
+    })
 }
