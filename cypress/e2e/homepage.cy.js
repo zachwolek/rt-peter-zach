2ec30df455d4e3605e85ea506b62773044025f21
Peter Kim <peterkim.pk1@gmail.com>
2024-07-10 20:33:58 -0400

    feat: created 500 server error test and passed test with correct error showing.
    Co-Authored-By: Zach Wolek <zach.wolek@gmail.com>
    Co-Authored-By: Peter Kim <peterkim.pk1@gmail.com>

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,4 +1,4 @@
-describe('template spec', () => {
+describe('Landing page tests', () => {
   beforeEach(() => {
     cy.fixture('mockMovieData').then((json) => {
       cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', json)
@@ -20,5 +20,17 @@ describe('template spec', () => {
   it('displayed movies should NOT be visible when last movie details are clicked', () => {
     cy.get('.card button').last().click()
     .get('.movies-wrapper').should('not.exist')
+  })
+
+})
+
+describe('Landing page API server error', () => {
+  it('should display a failed to fetch message if the server returns a 500 error', () => {
+    cy.intercept('GET','https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 500
+    }).as('getServerFailure')
+    .visit('http://localhost:3000')
+    .get('.app-title').should('contain.text', 'Rancid Tomatillos')
+    .get('h2').should('contain.text', 'HTTP Status Code 500: Internal Server Error')
   })
 })
