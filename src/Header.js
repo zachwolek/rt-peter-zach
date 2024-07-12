a498e5cc8202e197c95de5d862f63ed7d8fb37a0
Peter Kim <peterkim.pk1@gmail.com>
2024-07-12 18:27:25 -0400

    feat: created select dropdown with option values. working in progress to update option values with .map
    Co-Authored-By: Zach Wolek <zach.wolek@gmail.com>
    Co-Authored-By: Peter Kim <peterkim.pk1@gmail.com>

--- src/Header.js
+++ src/Header.js
@@ -1,56 +1,61 @@
 import './Header.css'
 import PropTypes from 'prop-types';
-
-export default function Header({movies, updateMovies}) {
+export default function Header({movies, updateMovies, filterMoviesByGenre}) {
     function handleFilter(input) {
         const matchingMovies = movies.map(movie => {
            return movie.title.toLowerCase().includes(input) ? {...movie} : {}
         })
+
     const filteredMovies = matchingMovies.filter(movie => Object.keys(movie).length !== 0)
     updateMovies(filteredMovies)
     }
-
+    function handleGenreFilter(selection) {
+        console.log("SELECTION: ", selection)
+        //Currently showing up and working ^^
+        const genreMovies = filterMoviesByGenre(selection)
+        console.log(genreMovies, 'line14')
+        //Currently not showing up
+    }
     return (
         <nav>
             <h1 className='app-title'>Rancid Tomatillos</h1>
             <div className='search-wrapper'>
                 <input placeholder='Search By Title'className='search' onChange={(e) => handleFilter(e.target.value)
                 }></input>
-                <button>Search</button>
             </div>
-        </nav>
+            <div className='genre-wrapper'>
+                <label for="genres">Choose a genre:</label>
+                <select id="genres" name="genres" onChange={(e) => handleGenreFilter(e.target.value)}>
+                    {/* {allGenres} */}
+                    
+                    <option value="Action">Action</option>
+                    <option value="Fantasy">Fantasy</option>
+                    <option value="Science Fiction">Science Fiction</option>
+                    <option value="Drama">Drama</option>
+                    <option value="History">History</option>
+                    <option value="Comedy">Comedy</option>
+                    <option value="Crime">Crime</option>
+                    <option value="Adventure">Adventure</option>
+                    <option value="Thriller">Thriller</option>
+                    <option value="Horror">Horror</option>
+                    <option value="Mystery">Mystery</option>
+                    <option value="Family">Family</option>
+                    <option value="Music">Music</option>
+                    <option value="Animation">Animation</option>
+                    <option value="War">War</option>
+                    <option value="Romance">Romance</option>
+                </select>
+            </div>
+     </nav>
     )
 }
 
-// genreValue on submit ("action")
-//filteredByGenre = []
-//
-// For each movie in movies array
-//do a GET request in getSingleMovie(movie.id)
-//if singleMovie.genre.includes(genreValue)
-//push the movie id into filteredByGenre
-//filter movies by array of ids by genre
-//setMovies state to array of [ids]
-
-//const allIds = [
-//     436270,  724495, 1013860, 505642,
-//     934641,  829799,  972313, 882598,
-//     830784,  982620,  663712, 985939,
-//     551271, 1037858,  979924,  49046,
-//     899112,  676701,  829280, 760204,
-//     361743,  844396,  897192, 593643,
-//     632856,  820067,  916605, 560052,
-//     848058,  807356,  635302, 315162,
-//     566466,  955644,  900667, 664469,
-//    1001835,  924482,  877269, 760104
-//  ]
-// .map over data to get ids
-// for each id, make a network request to get single movie details
-// for each single movie details, create array of objects that has title with genres
-// [{title: ['action','science fiction']},]
-// action
-
-
+// const allGenres = genres.map(genre => {
+//     return (
+//         <option value={genre}>{genre}</option>
+//     )
+// })
+// console.log(genres)
 
 
 Header.propTypes = {
@@ -62,5 +67,5 @@ Header.propTypes = {
         release_date: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired,
         backdrop_path: PropTypes.string.isRequired
-    })
+    }),
 }
